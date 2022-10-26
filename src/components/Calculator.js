02ee1fad280bee51d4983ff3f5e7818accb13f27
Miliyonayalew <miliayalew@gmail.com>
2022-10-26 19:59:51 +0300

    Add event listener and pass props to child components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,38 +1,53 @@
 import React from 'react';
 import './calculator.css';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (e) => {
+    const {
+      target: { textContent: buttonName },
+    } = e;
+    const result = calculate(this.state, buttonName);
+    this.setState(result);
   }
 
   render() {
+    const { total, next, operation } = this.state;
     return (
       <div className="calculator">
         <div className="container">
           <div className="display">
-            <p className="result">0</p>
+            <p className="result">{(total || '') + (operation || '') + (next || '') || 0}</p>
           </div>
-          <button type="button" className="btn">AC</button>
-          <button type="button" className="btn">+/-</button>
-          <button type="button" className="btn">%</button>
-          <button type="button" className="btn-end">÷</button>
-          <button type="button" className="btn">7</button>
-          <button type="button" className="btn">8</button>
-          <button type="button" className="btn">9</button>
-          <button type="button" className="btn-end">X</button>
-          <button type="button" className="btn">4</button>
-          <button type="button" className="btn">5</button>
-          <button type="button" className="btn">6</button>
-          <button type="button" className="btn-end">-</button>
-          <button type="button" className="btn">1</button>
-          <button type="button" className="btn">2</button>
-          <button type="button" className="btn">3</button>
-          <button type="button" className="btn-end">+</button>
-          <button type="button" className="btn-zero">0</button>
-          <button type="button" className="btn">.</button>
-          <button type="button" className="btn-end">=</button>
+          <button type="button" onClick={this.handleClick} className="btn">AC</button>
+          <button type="button" onClick={this.handleClick} className="btn">+/-</button>
+          <button type="button" onClick={this.handleClick} className="btn">%</button>
+          <button type="button" onClick={this.handleClick} className="btn-end">÷</button>
+          <button type="button" onClick={this.handleClick} className="btn">7</button>
+          <button type="button" onClick={this.handleClick} className="btn">8</button>
+          <button type="button" onClick={this.handleClick} className="btn">9</button>
+          <button type="button" onClick={this.handleClick} className="btn-end">x</button>
+          <button type="button" onClick={this.handleClick} className="btn">4</button>
+          <button type="button" onClick={this.handleClick} className="btn">5</button>
+          <button type="button" onClick={this.handleClick} className="btn">6</button>
+          <button type="button" onClick={this.handleClick} className="btn-end">-</button>
+          <button type="button" onClick={this.handleClick} className="btn">1</button>
+          <button type="button" onClick={this.handleClick} className="btn">2</button>
+          <button type="button" onClick={this.handleClick} className="btn">3</button>
+          <button type="button" onClick={this.handleClick} className="btn-end">+</button>
+          <button type="button" onClick={this.handleClick} className="btn-zero">0</button>
+          <button type="button" onClick={this.handleClick} className="btn">.</button>
+          <button type="button" onClick={this.handleClick} className="btn-end">=</button>
         </div>
       </div>
     );
