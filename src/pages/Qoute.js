fe5b3821eb3ba46b786d1537d8f428c9c40bbad6
Miliyonayalew <miliayalew@gmail.com>
2022-11-02 01:12:17 +0300

    Create Home and About page

---
+++ src/pages/Qoute.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Quote = () => (
+  <div className="quote">
+    <h1>
+      Mathematics is not about numbers, equations, computations, or algorithms: it is about
+      understanding. –William Paul Thurston.
+    </h1>
+  </div>
+);
+
+export default Quote;
