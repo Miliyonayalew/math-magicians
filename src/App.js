9f2f4ae0fba605d8b4ddc22e4ff89c29166dcf1c
Miliyonayalew <miliayalew@gmail.com>
2022-11-02 01:14:43 +0300

    create route for pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './pages/Home';
+import Quote from './pages/Qoute';
+import Navbar from './components/Navbar';
 
 const App = () => (
-  <div className="App">
-    <Calculator />
-  </div>
+  <>
+    <Navbar />
+    <Routes>
+      <Route
+        path="/"
+        element={<Home />}
+      />
+      <Route
+        path="/calculator"
+        element={(
+          <div className="App">
+            <h1>Let&apos;s do some math!</h1>
+            <Calculator />
+          </div>
+      )}
+      />
+      <Route path="/quote" element={<Quote />} />
+    </Routes>
+  </>
 );
 
 export default App;
